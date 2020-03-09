6b10a12369c6a9578493d1bfbbff9255a420c2da
Ricardo <ricardo.guia@atlanse.pt>
2020-03-09 16:47:46 +0000

    RicardoGuia21202

---
+++ Aula 3/exercicio4/RicardoGuia-213020/script.js
@@ -0,0 +1,15 @@
+var counterImages = 1;
+
+function addImage(){
+	var image = document.createElement("img");
+	var list = document.getElementById("imaagesList");
+
+	image.src = "https://placeimg.com/250/150/"+counterImages;
+	list.appendChild(image);
+
+	var counter = document.getElementById("imageCounter");
+	counter.innerText = counterImages.toString();
+
+	counterImages++;
+}
+
